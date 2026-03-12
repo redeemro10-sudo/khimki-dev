7a592129ff5715b06a73e946fbf041c70b4dbfa3
avrerii <avreriimarko@gmail.com>
2026-03-12 13:17:44 +0200

    fixed text

--- resources/views/partials/content-page.blade.php
+++ resources/views/partials/content-page.blade.php
@@ -1,14 +1,17 @@
 @php
     $id = get_the_ID();
     $pageText = $id ? get_post_meta($id, '_page_text', true) : '';
+    $contentClass = $contentClass ?? 'prose mt-2 max-w-none';
 @endphp
 
 @if (!empty($pageText))
-    <article class="prose mt-2 max-w-none">
+    <article class="{{ $contentClass }}">
         {!! $pageText !!}
     </article>
 @else
-    @php(the_content())
+    <article class="{{ $contentClass }}">
+        @php(the_content())
+    </article>
 @endif
 
 @if ($pagination())
--- resources/views/taxonomy.blade.php
+++ resources/views/taxonomy.blade.php
@@ -35,9 +35,13 @@
     ]" />
 
     {{-- Текст для терма или описание таксономии --}}
-    @if (trim($termText) !== '')
-        <div class="prose mt-2 max-w-none">{!! $termText !!}</div>
-    @else
-        {!! term_description($term) !!}
-    @endif
+    <section class="catalog-copy-wrap mx-auto mt-8 max-w-5xl px-4 pb-6 sm:px-6 lg:px-8">
+        <div class="catalog-copy rounded-[2rem] border border-slate-200 bg-white p-6 shadow-sm sm:p-8 lg:p-10">
+            @if (trim($termText) !== '')
+                {!! $termText !!}
+            @else
+                {!! term_description($term) !!}
+            @endif
+        </div>
+    </section>
 @endsection
