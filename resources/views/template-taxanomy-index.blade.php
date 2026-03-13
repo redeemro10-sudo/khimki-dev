babc6d3a607f4addd7877ccf154d74bf97b7d186
avrerii <avreriimarko@gmail.com>
2026-03-13 09:44:35 +0200

    ld-json markup

--- resources/views/template-taxanomy-index.blade.php
+++ resources/views/template-taxanomy-index.blade.php
@@ -46,5 +46,9 @@
         </p>
     @endif
 
-    @include('partials.content-page')
+    <section class="catalog-copy-wrap mx-auto mt-8 max-w-5xl px-4 pb-6 sm:px-6 lg:px-8">
+        <div class="catalog-copy rounded-[2rem] border border-slate-200 bg-white p-6 shadow-sm sm:p-8 lg:p-10">
+            @include('partials.content-page', ['contentClass' => 'catalog-copy'])
+        </div>
+    </section>
 @endsection
